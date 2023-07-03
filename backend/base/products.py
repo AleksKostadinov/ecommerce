6288a77df45c144c811a0895cd2d77d7b26e50e1
AleksKostadinov <kostadinov12@gmail.com>
2023-07-03 23:57:05 +0300

    Implementing Redux for State managment

--- backend/base/products.py
+++ backend/base/products.py
@@ -21,7 +21,7 @@ products = [
         'brand': 'Apple',
         'category': 'Electronics',
         'price': 599.99,
-        'countInStock': 0,
+        'countInStock': 5,
         'rating': 4.0,
         'numReviews': 8,
     },
