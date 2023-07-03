c7603741485776e752a4747b4a04678e230e04ec
AleksKostadinov <kostadinov12@gmail.com>
2023-07-03 12:43:02 +0300

    Implementing React Router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
-  return (
-    <header>
-        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-            <Container>
-                <Navbar.Brand href="/">eShop</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="/cart"><i className='fas fa-shopping-cart'>Cart</i></Nav.Link>
-                    <Nav.Link href="/login"><i className='fas fa-user'>Login</i></Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    </header>
-  )
-}
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
 
-export default Header
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>eShop</Navbar.Brand>
+                    </LinkContainer>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <i className="fas fa-shopping-cart">Cart</i>
+                                </Nav.Link>
+                            </LinkContainer>
+
+                            <LinkContainer to='/login'>
+                                <Nav.Link>
+                                    <i className="fas fa-user">Login</i>
+                                </Nav.Link>
+                            </LinkContainer>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+};
+
+export default Header;
