3ac44e87c0f2a6d27d17232835a172144295d8f3
AleksKostadinov <kostadinov12@gmail.com>
2023-07-10 12:37:42 +0300

    Create Review Endpoint

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -4,8 +4,11 @@ from base.views import product_views as views
 
 urlpatterns = [
     path('', views.get_products, name='products'),
+
     path('create/', views.create_product, name='product-create'),
     path('upload/', views.upload_image, name='image-upload'),
+
+    path('<str:pk>/reviews/', views.create_product_review, name='create-review'),
     path('<str:pk>/', views.get_product, name='product'),
 
 
